a54bb3ef089cd7c051b2e81d0ffae117c484cd9e
Siddhant Gupta <siddhantgupta792000@gmail.com>
2024-09-28 15:41:57 +1000

    Task 10.2HD

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,15 +3,6 @@
 GKE_CLUSTER_NAME="task10"
 GKE_REGION="europe-west1"
 
-TEMP_KEY_FILE="cred.json"
-echo "${GCP_SA_KEY}" > ${TEMP_KEY_FILE}
-
-echo "Logging into Google Cloud..."
-gcloud auth activate-service-account --key-file=${TEMP_KEY_FILE}
-
-echo "Retrieving GKE credentials..."
-gcloud container clusters get-credentials $GKE_CLUSTER_NAME --region $GKE_REGION
-
 echo "Creating secret for image pulling..."
 kubectl create secret docker-registry gcr-secret \
   --docker-server=https://gcr.io \
